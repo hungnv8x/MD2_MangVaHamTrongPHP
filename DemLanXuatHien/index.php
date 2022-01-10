327a91e548c75a744eb25cb9bbbbbb9c3553d3c7
Hưng Nguyễn Văn <hungnvhb1988@gmail.com>
2022-01-10 14:05:15 +0700

    DemLanXuatHien

---
+++ DemLanXuatHien/index.php
@@ -0,0 +1,14 @@
+<?php
+function demKyTu($str, $x)
+{
+    $count = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] == $x) {
+            $count++;
+        }
+    }
+    return $count;
+}
+$str="abcdabcaa";
+$x = "a";
+echo demKyTu($str,$x);
